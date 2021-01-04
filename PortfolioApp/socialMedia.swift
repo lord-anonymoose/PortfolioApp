48ab7c6ead01d0170b3a77a46024f844f27d9238
Philipp <severus99@icloud.com>
2021-01-04 16:27:29 +0300

    Sending email feature works fine

--- PortfolioApp/socialMedia.swift
+++ PortfolioApp/socialMedia.swift
@@ -9,21 +9,43 @@ import Foundation
 import SwiftUI
 import MessageUI
 
-func sendEmail() {
-    if MFMailComposeViewController.canSendMail() {
-        let mail = MFMailComposeViewController()
-        mail.mailComposeDelegate = self
-        mail.setToRecipients([mailAddress])
-        mail.setMessageBody("", isHTML: true)
-        
-        present(mail, animated: true)
-    } else {
-        
+struct MailComposeViewController: UIViewControllerRepresentable {
+    
+    var toRecipients: [String]
+    var mailBody: String
+    
+    var didFinish: ()->()
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
     }
-}
-
-func mailComposeController (_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeError, error: Error) {
-    controller.dismiss(animated: true)
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailComposeViewController>) -> MFMailComposeViewController {
+        
+        let mail = MFMailComposeViewController()
+        mail.mailComposeDelegate = context.coordinator
+        mail.setToRecipients(self.toRecipients)
+        mail.setMessageBody(self.mailBody, isHTML: true)
+        
+        return mail
+    }
+    
+    final class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+        
+        var parent: MailComposeViewController
+        
+        init(_ mailController: MailComposeViewController) {
+            self.parent = mailController
+        }
+        
+        func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+            parent.didFinish()
+            controller.dismiss(animated: true)
+        }
+    }
+    
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController, context: UIViewControllerRepresentableContext<MailComposeViewController>) {
+    }
 }
 
 struct mailIcon: View {
@@ -34,6 +56,7 @@ struct mailIcon: View {
 
 struct mediaIcon: View {
     @Environment(\.openURL) var openURL
+    @State private var showingMail = false
     var image: Image
     var link: URL
     init (image: Image, link: URL) {
@@ -45,7 +68,16 @@ struct mediaIcon: View {
             .resizable()
             .frame(width: 40, height: 40)
             .onTapGesture {
-                openURL(link)
+                if image == mailImage {
+                    self.showingMail.toggle()
+                } else {
+                    openURL(link)
+                }
+            }
+            .sheet(isPresented: $showingMail) {
+                        MailComposeViewController(toRecipients: [mailAddress], mailBody: "") {
+                            // Did finish action
+                        }
             }
             .padding(.leading, 10)
             .padding(.trailing, 10)
@@ -59,6 +91,7 @@ struct socialMediaBar: View {
             mediaIcon(image: linkedinImage, link: linkedinURL!)
             mediaIcon(image: twitterImage, link: twitterURL!)
             mediaIcon(image: telegramImage, link: telegramURL!)
+            mediaIcon(image: mailImage, link: mailURL!)
         }
     }
 }
@@ -73,4 +106,5 @@ let githubURL = URL(string: "https://github.com/lord-anonymoose")
 let twitterURL = URL(string: "https://twitter.com/severus_")
 let linkedinURL = URL(string: "https://www.linkedin.com/in/philipp-lazarev-782b14167")
 let telegramURL = URL(string: "https://t.me/strawberrymoose")
+let mailURL = URL(string:"https://icloud.com")
 let mailAddress = "severus99@icloud.com"
